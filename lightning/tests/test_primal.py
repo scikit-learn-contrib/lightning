c765d1e7e8af81d5a09b53e592d768d7318c963a
Mathieu Blondel <mathieu@mblondel.org>
2012-01-13 14:58:52 +0900

    Better test.

--- lightning/tests/test_primal.py
+++ lightning/tests/test_primal.py
@@ -6,32 +6,34 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal, \
 from nose.tools import assert_raises, assert_true
 
 from sklearn.svm import LinearSVC
+from sklearn.svm.sparse import LinearSVC as SparseLinearSVC
 from sklearn.datasets import load_iris
 from sklearn.datasets.samples_generator import make_classification
 from sklearn.utils import check_random_state
 
 from lightning.primal import PrimalClassifier
 
-iris = load_iris()
-random_state = check_random_state(0)
-perm = random_state.permutation(iris.target.size)
-iris_dense = iris.data[perm]
-iris_target = iris.target[perm]
-iris_sparse = sp.csr_matrix(iris_dense)
+bin_dense, bin_target = make_classification(n_samples=200, n_features=100,
+                                            n_informative=5,
+                                            n_classes=2, random_state=0)
+bin_sparse = sp.csr_matrix(bin_dense)
 
-gen_dense, gen_target = make_classification(n_samples=200, n_features=100,
-                                            random_state=0)
-gen_sparse = sp.csr_matrix(gen_dense)
+mult_dense, mult_target = make_classification(n_samples=300, n_features=100,
+                                              n_informative=5,
+                                              n_classes=3, random_state=0)
+mult_sparse = sp.csr_matrix(mult_dense)
+
 
 def test_primal_fit_binary():
-    for X in (gen_dense, gen_sparse):
+    for X in (bin_dense, bin_sparse):
+        # Caution: use a dense LinearSVC even on sparse data!
         clf = PrimalClassifier(LinearSVC())
-        y_pred = clf.fit(X, gen_target).predict(X)
-        assert_true(np.mean(y_pred == gen_target) >= 0.95)
+        y_pred = clf.fit(X, bin_target).predict(X)
+        assert_true(np.mean(y_pred == bin_target) >= 0.98)
 
 
 def test_primal_fit_multiclass():
-    for X in (iris_dense, iris_sparse):
+    for X in (mult_dense, mult_sparse):
         clf = PrimalClassifier(LinearSVC())
-        y_pred = clf.fit(X, iris_target).predict(X)
-        assert_true(np.mean(y_pred == iris_target))
+        y_pred = clf.fit(X, mult_target).predict(X)
+        assert_true(np.mean(y_pred == mult_target) >= 0.8)
