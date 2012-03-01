4d9c1aeab43abaa5a805191d5794a1b55e519908
Mathieu Blondel <mathieu@mblondel.org>
2012-03-01 16:19:25 +0900

    Trim model at the end.

--- lightning/dual_cd.py
+++ lightning/dual_cd.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.utils import check_random_state
+from sklearn.utils import check_random_state, safe_mask
 from sklearn.metrics.pairwise import pairwise_kernels
 
 from dual_cd_fast import _dual_cd
@@ -68,23 +68,35 @@ class DualSVC(BaseEstimator):
     def fit(self, X, y):
         n_samples = X.shape[0]
         rs = check_random_state(self.random_state)
+
         self.label_binarizer_ = LabelBinarizer(neg_label=-1, pos_label=1)
         Y = self.label_binarizer_.fit_transform(y)
         n_vectors = Y.shape[1]
+
         K = pairwise_kernels(X, X, metric=self.kernel,
                              filter_params=True, n_jobs=self.n_jobs,
                              **self._kernel_params())
         self.dual_coef_ = np.zeros((n_vectors, n_samples), dtype=np.float64)
+
         for i in xrange(n_vectors):
             _dual_cd(self.dual_coef_[i], K, Y[:, i],
                      self.C, self.loss, self.max_iter, rs, self.tol,
                      precomputed_kernel=True, verbose=self.verbose)
+
         self.dual_coef_ *= Y.T
-        # FIXME: can trim the model
-        self.X_train_ = X
+
+        sv = np.sum(self.dual_coef_ != 0, axis=0, dtype=bool)
+        self.dual_coef_ = self.dual_coef_[:, sv]
+        mask = safe_mask(X, sv)
+        self.support_vectors_ = X[mask]
+
+        if self.verbose >= 1:
+            print "Number of support vectors:", np.sum(sv)
 
     def decision_function(self, X):
-        K = np.dot(X, self.X_train_.T)
+        K = pairwise_kernels(X, self.support_vectors_, metric=self.kernel,
+                             filter_params=True, n_jobs=self.n_jobs,
+                             **self._kernel_params())
         return np.dot(K, self.dual_coef_.T)
 
     def predict(self, X):
