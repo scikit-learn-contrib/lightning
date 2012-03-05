67f97efc94c4a4d9d5e10112bc78c4b766529c35
Mathieu Blondel <mathieu@mblondel.org>
2012-03-05 20:02:02 +0900

    Add test for LaSVM.

---
+++ lightning/tests/test_lasvm.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.sparse as sp
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal, \
+                          assert_almost_equal
+from nose.tools import assert_raises, assert_true, assert_equal
+
+from sklearn.datasets.samples_generator import make_classification
+
+from lightning.lasvm import LaSVM
+
+bin_dense, bin_target = make_classification(n_samples=200, n_features=100,
+                                            n_informative=5,
+                                            n_classes=2, random_state=0)
+bin_sparse = sp.csr_matrix(bin_dense)
+
+mult_dense, mult_target = make_classification(n_samples=300, n_features=100,
+                                              n_informative=5,
+                                              n_classes=3, random_state=0)
+mult_sparse = sp.csr_matrix(mult_dense)
+
+
+def test_fit_rbf_binary():
+        clf = LaSVM(random_state=0, max_iter=2, kernel="rbf")
+        clf.fit(bin_dense, bin_target)
+        acc = clf.score(bin_dense, bin_target)
+        assert_almost_equal(acc, 1.0)
