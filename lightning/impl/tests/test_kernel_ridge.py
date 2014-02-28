63bf390e13286afe71658e7b3b49e193848a3b9b
Mathieu Blondel <mathieu@mblondel.org>
2014-02-28 17:03:13 +0900

    Add assertion.

--- lightning/impl/tests/test_kernel_ridge.py
+++ lightning/impl/tests/test_kernel_ridge.py
@@ -28,3 +28,7 @@ def test_kernel_ridge_multi_output():
     pred = Ridge(alpha=1, fit_intercept=False).fit(X, Y).predict(X)
     pred2 = KernelRidge(kernel="linear", alpha=1).fit(X, Y).predict(X)
     assert_array_almost_equal(pred, pred2)
+
+    pred3 = KernelRidge(kernel="linear", alpha=1).fit(X, y).predict(X)
+    pred3 = np.array([pred3, pred3]).T
+    assert_array_almost_equal(pred2, pred3)
