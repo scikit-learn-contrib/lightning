1c1b85686b9744f988d05ecdbaca20c198488e7b
Mathieu Blondel <mathieu@mblondel.org>
2014-06-21 03:43:52 +0900

    Add Cython implementation of PRank.

--- lightning/impl/prank.py
+++ lightning/impl/prank.py
@@ -5,6 +5,12 @@ import numpy as np
 
 from sklearn.base import BaseEstimator
 from sklearn.preprocessing import LabelEncoder
+from sklearn.utils.extmath import safe_sparse_dot
+
+from .dataset_fast import get_dataset
+from .prank_fast import _prank_fit
+from .prank_fast import _prank_predict
+
 
 class _BasePRank(BaseEstimator):
 
@@ -18,18 +24,6 @@ class PRank(_BasePRank):
     def __init__(self, n_iter=10):
         self.n_iter = n_iter
 
-    def _predit(self, x):
-        pred = np.dot(x, self.coef_)
-        n_classes = len(self.thresholds_)
-        y_hat = 0
-
-        for r in xrange(n_classes):
-            if pred - self.thresholds_[r] < 0:
-                y_hat = r
-                break
-
-        return y_hat, pred
-
     @property
     def classes_(self):
         return self.label_encoder_.classes_
@@ -38,41 +32,22 @@ class PRank(_BasePRank):
         n_samples, n_features = X.shape
 
         self.label_encoder_ = LabelEncoder()
-        y = self.label_encoder_.fit_transform(y)
+        y = self.label_encoder_.fit_transform(y).astype(np.int32)
         n_classes = len(self.classes_)
 
         self.coef_ = np.zeros(n_features, dtype=np.float64)
         self.thresholds_ = np.zeros(n_classes, dtype=np.float64)
         self.thresholds_[-1] = np.inf
-        yr = np.ones(n_classes)
 
-        for n in xrange(self.n_iter):
-            for i in xrange(n_samples):
+        ds = get_dataset(X)
 
-                y_hat, pred = self._predit(X[i])
-
-                if y_hat == y[i]:
-                    continue
-
-                for r in xrange(n_classes - 1):
-                    if y[i] <= r:
-                        yr[r] = -1
-                    else:
-                        yr[r] = 1
-
-                tau = 0
-                for r in xrange(n_classes - 1):
-                    if yr[r] * (pred - self.thresholds_[r]) <= 0:
-                        tau += yr[r]
-                        self.thresholds_[r] -= yr[r]
-
-                self.coef_ += tau * X[i]
+        _prank_fit(self.coef_, self.thresholds_, ds, y, n_classes, self.n_iter)
 
         return self
 
     def predict(self, X):
         n_samples = X.shape[0]
-        pred = np.zeros(X.shape[0], dtype=int)
-        for i in xrange(n_samples):
-            pred[i] = self._predit(X[i])[0]
-        return self.label_encoder_.inverse_transform(pred)
+        dot = safe_sparse_dot(X, self.coef_)
+        out = np.zeros(n_samples, dtype=np.int32)
+        _prank_predict(dot, self.thresholds_, len(self.classes_), out)
+        return self.label_encoder_.inverse_transform(out)
---
+++ lightning/impl/prank_fast.pyx
@@ -0,0 +1,90 @@
+# encoding: utf-8
+# cython: cdivision=True
+# cython: boundscheck=False
+# cython: wraparound=False
+#
+# Author: Mathieu Blondel
+# License: BSD
+
+import numpy as np
+cimport numpy as np
+
+from lightning.impl.dataset_fast cimport RowDataset
+
+
+cdef int _predict(double dot,
+                  np.ndarray[double, ndim=1, mode='c'] b,
+                  int n_classes):
+    cdef int r
+    cdef int y_hat = 0
+
+    for r in xrange(n_classes):
+        if dot - b[r] < 0:
+            y_hat = r
+            break
+
+    return y_hat
+
+
+def _prank_fit(np.ndarray[double, ndim=1, mode='c'] w,
+               np.ndarray[double, ndim=1, mode='c'] b,
+               RowDataset X,
+               np.ndarray[int, ndim=1] y,
+               int n_classes,
+               int n_iter):
+
+    cdef int n_samples = X.get_n_samples()
+    cdef int n_features = X.get_n_features()
+
+    cdef int n, i, j, jj, y_hat, tau, yr, r
+    cdef double dot
+
+    # Data pointers.
+    cdef double* data
+    cdef int* indices
+    cdef int n_nz
+
+    for n in xrange(n_iter):
+        for i in xrange(n_samples):
+            # Retrieve row.
+            X.get_row_ptr(i, &indices, &data, &n_nz)
+
+            # Compute dot product.
+            dot = 0
+            for jj in xrange(n_nz):
+                j = indices[jj]
+                dot += w[j] * data[jj]
+
+            y_hat = _predict(dot, b, n_classes)
+
+            # Nothing to do if prediction was correct.
+            if y_hat == y[i]:
+                continue
+
+            tau = 0
+            for r in xrange(n_classes - 1):
+                if y[i] <= r:
+                    yr = -1
+                else:
+                    yr = 1
+
+                if yr * (dot - b[r]) <= 0:
+                    tau += yr
+                    b[r] -= yr
+
+            # Update w.
+            for jj in xrange(n_nz):
+                j = indices[jj]
+                w[j] += tau * data[jj]
+
+
+def _prank_predict(np.ndarray[double, ndim=1, mode='c'] dot,
+                   np.ndarray[double, ndim=1, mode='c'] b,
+                   int n_classes,
+                   np.ndarray[int, ndim=1, mode='c'] out):
+
+    cdef int n_samples = dot.shape[0]
+    cdef int i
+
+    for i in xrange(n_samples):
+        out[i] = _predict(dot[i], b, n_classes)
