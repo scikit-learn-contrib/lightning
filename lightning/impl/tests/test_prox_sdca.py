a91d3b6398a134cfd03f1baf1cb549d2b5cef682
Mathieu Blondel <mathieu@mblondel.org>
2014-11-11 23:45:32 +0900

    Test elastic-net case.

--- lightning/impl/tests/test_prox_sdca.py
+++ lightning/impl/tests/test_prox_sdca.py
@@ -26,3 +26,9 @@ def test_sdca_absolute():
     clf = ProxSDCA_Classifier(loss="absolute")
     clf.fit(X, y)
     assert_equal(clf.score(X, y), 1.0)
+
+
+def test_prox_sdca_hinge():
+    clf = ProxSDCA_Classifier(alpha=0.5, l1_ratio=0.85, loss="hinge")
+    clf.fit(X, y)
+    assert_equal(clf.score(X, y), 1.0)
