5df946415fa34fda36fd043fabeec2bf7477219e
Mathieu Blondel <mathieu@mblondel.org>
2015-01-01 23:03:40 +0900

    Add SVRG benchmark.

---
+++ benchmarks/bench_svrg.py
@@ -0,0 +1,19 @@
+import time
+
+import numpy as np
+
+from sklearn.datasets import fetch_20newsgroups_vectorized
+from lightning.classification import SVRGClassifier
+
+bunch = fetch_20newsgroups_vectorized(subset="all")
+X = bunch.data
+y = bunch.target
+y[y >= 1] = 1
+
+clf = SVRGClassifier(alpha=1e-3, max_iter=5, verbose=1, random_state=0)
+start = time.time()
+clf.fit(X, y)
+
+print "Training time", time.time() - start
+print "Accuracy", np.mean(clf.predict(X) == y)
+print "% non-zero", clf.n_nonzero(percentage=True)
