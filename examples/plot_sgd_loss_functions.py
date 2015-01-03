cb9c7d1e7f35197ffd9d4ab5685dc7e024d80c21
Mathieu Blondel <mathieu@mblondel.org>
2015-01-03 21:14:52 +0900

    Update loss example.

--- examples/plot_sgd_loss_functions.py
+++ examples/plot_sgd_loss_functions.py
@@ -9,18 +9,16 @@ print __doc__
 import numpy as np
 import pylab as pl
 from lightning.impl.sgd import Hinge
+from lightning.impl.sgd import SquaredHinge
 from lightning.impl.sgd import Log
-from lightning.impl.sgd import SparseLog
-from lightning.impl.sgd import ModifiedHuber
 from lightning.impl.sgd import SquaredLoss
 
 ###############################################################################
 # Define loss funcitons
 xmin, xmax = -3, 3
 hinge = Hinge(1)
+squared_hinge = SquaredHinge()
 log = Log()
-sparse_log = SparseLog()
-modified_huber = ModifiedHuber()
 squared_loss = SquaredLoss()
 
 ###############################################################################
@@ -30,14 +28,12 @@ pl.plot([xmin, 0, 0, xmax], [1, 1, 0, 0], 'k-',
         label="Zero-one loss")
 pl.plot(xx, [hinge.loss(x, 1) for x in xx], 'g-',
         label="Hinge loss")
+pl.plot(xx, [squared_hinge.loss(x, 1) for x in xx], 'b--',
+        label="Squared hinge loss", zorder=3)
 pl.plot(xx, [log.loss(x, 1) for x in xx], 'r-',
         label="Log loss")
-pl.plot(xx, [sparse_log.loss(x, 1) for x in xx], 'm-',
-        label="SparseLog loss")
-pl.plot(xx, [modified_huber.loss(x, 1) for x in xx], 'y-',
-        label="Modified huber loss")
-#pl.plot(xx, [2.0*squared_loss.loss(x, 1) for x in xx], 'c-',
-#        label="Squared loss")
+pl.plot(xx, [2.0*squared_loss.loss(x, 1) for x in xx], 'c-',
+       label="Squared loss")
 pl.ylim((0, 5))
 pl.legend(loc="upper right")
 pl.xlabel(r"$y \cdot f(x)$")
