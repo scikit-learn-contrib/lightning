03737c3c61c5beefd0d18161bcd0f51c8dfc5501
Mathieu Blondel <mathieu@mblondel.org>
2015-01-23 23:38:16 +0900

    Add bench_amazon7.py.

---
+++ benchmarks/bench_amazon7.py
@@ -0,0 +1,24 @@
+import sys
+
+from sklearn.externals import joblib
+
+from lightning.classification import SDCAClassifier
+
+if len(sys.argv) == 1:
+    print """
+    Please enter the path to amazon7_uncompressed_pkl/ folder.
+
+    Download data from
+    http://www.mblondel.org/data/amazon7_uncompressed_pkl.tar.bz2
+    """
+    exit()
+
+data = joblib.load(sys.argv[1], mmap_mode="r")
+X = data["X"]
+y = data["y"].copy()  # copy is needed to modify y.
+
+y[y >= 1] = 1  # Create a binary classification problem.
+
+clf = SDCAClassifier(tol=1e-5, max_iter=10, verbose=1)
+clf.fit(X, y)
+print clf.score(X, y)
