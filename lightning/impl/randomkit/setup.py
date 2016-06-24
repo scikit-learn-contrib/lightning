f6fb285b51b85913156b4f3b0e85981cd31c693f
Fabian Pedregosa <f@bianp.net>
2016-06-24 11:04:33 -0400

    Add random_fast.pxd to install path.

--- lightning/impl/randomkit/setup.py
+++ lightning/impl/randomkit/setup.py
@@ -17,7 +17,7 @@ def configuration(parent_package='', top_path=None):
          )
 
     config.add_subpackage('tests')
-
+    config.add_data_files('random_fast.pxd')
     return config
 
 if __name__ == '__main__':
