21e07117d3c8fc323d477a3a2c021cd8f34e9651
Mathieu Blondel <mathieu@mblondel.org>
2016-10-18 21:02:29 +0900

    Add FISTA to plot.

--- examples/plot_l1_reg.py
+++ examples/plot_l1_reg.py
@@ -16,6 +16,7 @@ from sklearn.model_selection import train_test_split
 
 from lightning.regression import CDRegressor
 from lightning.regression import FWRegressor
+from lightning.regression import FistaRegressor
 
 diabetes = load_diabetes()
 X, y = diabetes.data, diabetes.target
@@ -29,6 +30,8 @@ alphas = np.logspace(-4, 4, 50)
 
 fw_n_nz = []
 fw_error = []
+fista_n_nz = []
+fista_error = []
 cd_n_nz = []
 cd_error = []
 
@@ -39,6 +42,13 @@ for beta in betas:
     fw_n_nz.append(np.sum(reg.coef_ != 0))
     fw_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
 
+    if beta <= 1000:
+        reg = FistaRegressor(penalty="l1-ball", alpha=beta, max_iter=1000, verbose=0)
+        reg.fit(X_tr, y_tr)
+        y_pred = reg.predict(X_te)
+        fista_n_nz.append(np.sum(reg.coef_ != 0))
+        fista_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
+
 for alpha in alphas:
     reg = CDRegressor(alpha=alpha, penalty="l1", max_iter=1000, tol=1e-3,
                       verbose=0)
@@ -47,7 +57,10 @@ for alpha in alphas:
     cd_n_nz.append(np.sum(reg.coef_ != 0))
     cd_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
 
+#fista_error = np.array(fista_error)[np.argsort(fista_n_nz)]
+
 plt.plot(fw_n_nz, fw_error, label="Frank-Wolfe", linewidth=3)
+plt.plot(fista_n_nz, fista_error, label="FISTA", linewidth=3, marker="s")
 plt.plot(cd_n_nz, cd_error, label="Coordinate Descent", linewidth=3, linestyle="--")
 
 plt.xlabel("Number of non-zero coefficients")
