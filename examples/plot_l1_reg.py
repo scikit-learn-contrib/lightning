27500f08aff37cb8c4a33a91dbaf9ea9f6719866
Mathieu Blondel <mathieu@mblondel.org>
2017-01-18 13:10:43 +0900

    Use abs(x) > 1e-9 instead of x != 0.

--- examples/plot_l1_reg.py
+++ examples/plot_l1_reg.py
@@ -39,28 +39,28 @@ for beta in betas:
     reg = FWRegressor(beta=beta, max_iter=1000, tol=1e-3, verbose=0)
     reg.fit(X_tr, y_tr)
     y_pred = reg.predict(X_te)
-    fw_n_nz.append(np.sum(reg.coef_ != 0))
+    fw_n_nz.append(np.sum(np.abs(reg.coef_) > 1e-9))
     fw_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
 
-    if beta <= 1000:
-        reg = FistaRegressor(penalty="l1-ball", alpha=beta, max_iter=1000, verbose=0)
-        reg.fit(X_tr, y_tr)
-        y_pred = reg.predict(X_te)
-        fista_n_nz.append(np.sum(reg.coef_ != 0))
-        fista_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
+    reg = FistaRegressor(penalty="l1-ball", alpha=beta, max_iter=1000, verbose=0)
+    reg.fit(X_tr, y_tr)
+    y_pred = reg.predict(X_te)
+    fista_n_nz.append(np.sum(np.abs(reg.coef_) > 1e-9))
+    fista_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
 
 for alpha in alphas:
     reg = CDRegressor(alpha=alpha, penalty="l1", max_iter=1000, tol=1e-3,
                       verbose=0)
     reg.fit(X_tr, y_tr)
     y_pred = reg.predict(X_te)
-    cd_n_nz.append(np.sum(reg.coef_ != 0))
+    cd_n_nz.append(np.sum(np.abs(reg.coef_) > 1e-9))
     cd_error.append(np.sqrt(np.mean((y_te - y_pred) ** 2)))
 
 #fista_error = np.array(fista_error)[np.argsort(fista_n_nz)]
 
 plt.plot(fw_n_nz, fw_error, label="Frank-Wolfe", linewidth=3)
-plt.plot(fista_n_nz, fista_error, label="FISTA", linewidth=3, marker="s")
+plt.plot(fista_n_nz, fista_error, label="FISTA", linewidth=3, marker="s",
+         linestyle="--")
 plt.plot(cd_n_nz, cd_error, label="Coordinate Descent", linewidth=3, linestyle="--")
 
 plt.xlabel("Number of non-zero coefficients")
