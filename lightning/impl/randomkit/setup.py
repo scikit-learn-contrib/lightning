f54f0415c30ba51805d91292c6f3205955de9560
Loïc Estève <loic.esteve@ymail.com>
2017-02-07 16:24:26 +0100

    Build directly from cython sources
    Remove .cpp files from repo

--- lightning/impl/randomkit/setup.py
+++ lightning/impl/randomkit/setup.py
@@ -9,17 +9,18 @@ def configuration(parent_package='', top_path=None):
     if sys.platform == 'win32':
         libs.append('Advapi32')
 
-
-    config.add_extension('random_fast',
-         sources=['random_fast.cpp', 'randomkit.c'],
-         libraries=libs,
-         include_dirs=[numpy.get_include()]
-         )
+    config.add_extension(
+        'random_fast',
+        sources=['random_fast.pyx', 'randomkit.c'],
+        language='c++',
+        libraries=libs,
+        include_dirs=[numpy.get_include()]
+    )
 
     config.add_subpackage('tests')
     config.add_data_files('random_fast.pxd')
     config.add_data_files('randomkit.h')
-							
+
     return config
 
 if __name__ == '__main__':
