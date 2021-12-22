31c8ba90c0e44323946cdcf5069d04c8859b5e45
Nikita Titov <nekit94-12@hotmail.com>
2021-12-22 05:03:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(os.path.join('lightning', '__init__.py'), encoding='utf-8') as f:
     match = re.search(r'__version__[ ]*=[ ]*[\"\'](?P<version>.+)[\"\']',
                       f.read())
     VERSION = match.group('version').strip()
-MIN_PYTHON_VERSION = '3.6'
+MIN_PYTHON_VERSION = '3.7'
 with open('requirements.txt', encoding='utf-8') as f:
     REQUIREMENTS = [
         line.strip()
