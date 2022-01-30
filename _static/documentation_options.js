dc429e132a2d00dcce808686567627bf5a888c44
StrikerRUS <nekit94-08@mail.ru>
2022-01-30 00:40:03 +0000

    Deploying to gh-pages from @ scikit-learn-contrib/lightning@d3e3008f8f53f15d080fc958ec8cc873de8d993c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.2',
+    VERSION: '0.6.2.post0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
